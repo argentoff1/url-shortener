0e28f200d085cfc0667a9a4bd7c0f7436ea46f55
argentoff1 <mparinos56@gmail.com>
2026-04-24 13:39:03 +0300

    Инициализация slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 // Файл, который запускает программу
@@ -10,10 +18,37 @@ func main() {
 	cfg := config.MustLoad()
 
 	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+
+	// Постоянный вывод наименования окружения при запуске
+	// log = log.With(slog.String("env", cfg.Env))
+
+	// Будет выведено какое окружение используется при запуске программы
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	// Реализация разного уровня логирования для определенных окружений
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 type Config struct {
 	Env string `yaml:"env" env-default:"local"`
 	// При ошибке можно удалить env-required:"true"
-	StoragePath string `yaml:"storage_path" env-required:"true"`
+	StoragePath string `yaml:"storage_path"`
 	HTTPServer  `yaml:"http_server"`
 }
 
